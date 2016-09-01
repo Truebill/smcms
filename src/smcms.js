ecbf670d7fecba2d728f0da302038170b8b2efa3
Idris Mokhtarzada <idris.m@gmail.com>
2016-09-01 10:59:27 -0400

    add support for renderers (for example, markdown)

--- src/smcms.js
+++ src/smcms.js
@@ -1,12 +1,23 @@
-const defaultOptions = {};
+const defaultOptions = {
+  renderer: value => value,
+};
 
 export default class SMCMS {
   constructor(opts) {
     const options = Object.assign({}, defaultOptions, opts);
     this.store = options.store;
+    this.renderer = options.renderer;
+  }
+
+  rawValueToString(value) {
+    this.renderer(value);
+  }
+
+  getRawValue(key) {
+    return this.store.getValue(key);
   }
 
   getValue(key) {
-    return this.store.getValue(key);
+    return this.rawValueToString(this.getRawValue(key));
   }
 }
