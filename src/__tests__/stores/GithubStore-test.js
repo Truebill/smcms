0b3c659dc5a260116e3cbb5e595c7e023115a3fc
Steve <steve@truebill.com>
2016-09-19 09:37:47 -0700

    Extend test timeout to 10s for Github

--- src/__tests__/stores/GithubStore-test.js
+++ src/__tests__/stores/GithubStore-test.js
@@ -2,6 +2,7 @@ import { expect } from 'chai';
 import GithubStore from '../../stores/GithubStore';
 
 const { before, describe, it } = global;
+const GITHUB_TIMEOUT_MS = 10 * 1000;
 
 describe('GithubStore', () => {
   it('sets rootPath', async () => {
@@ -30,18 +31,18 @@ describe('GithubStore', () => {
       const value = await store.getValue('test1/simple.md');
 
       expect(value).to.equal('Here is some **content**!');
-    });
+    }).timeout(GITHUB_TIMEOUT_MS);
 
     it('returns null if file at path does not exist', async () => {
       const value = await store.getValue('test1/nothing');
 
       expect(value).to.be.null;
-    });
+    }).timeout(GITHUB_TIMEOUT_MS);
 
     it('does not decode if passed an option', async () => {
       const value = await store.getValue('test1/simple.md', { decode: null });
 
       expect(value).to.equal('SGVyZSBpcyBzb21lICoqY29udGVudCoqIQo=');
-    });
+    }).timeout(GITHUB_TIMEOUT_MS);
   });
 });
